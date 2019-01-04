ce16fbf9f9480cd1049c60aa0cfd7b2881b84d3f
rashika1495 <45651367+rashika1495@users.noreply.github.com>
2019-01-04 13:57:45 +0530

    changes

--- angular-web-app/src/app/views/login/login.component.ts
+++ angular-web-app/src/app/views/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Router} from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -15,9 +15,12 @@ export class LoginComponent implements OnInit {
   rememberMe:boolean=false;
   login: LoginData;
   errorMessage:string="";
+  private heroesUrl = 'https://localhost:44358/api/Login/GoogleLogin';
+ 
 
 
-constructor(private http:HttpClient, private router: Router,private toastr: ToastrService) { }
+constructor(private http:HttpClient, private router: Router,private toastr: ToastrService) {
+ }
 
 ngOnInit() { } 
 
@@ -27,7 +30,7 @@ async loginbuttonClick() {
     password:this.password
   }
 
- await this.http.post<LoginData>('https://localhost:44358/api/Login/Login',requestData ).subscribe(res => {
+ await this.http.post<LoginData>('http://net.site4demo.com/webapp/api/Login/Login',requestData ).subscribe(res => {
     this.login = res;
     console.log(this.login);
     if(this.login.message=="Success"){
@@ -40,6 +43,38 @@ async loginbuttonClick() {
   });
   
 }
+async googleLogin(){
+const headers = new HttpHeaders();
+headers.append('Access-Control-Allow-Headers', 'Content-Type');
+headers.append('Access-Control-Allow-Methods', 'GET');
+headers.set('Access-Control-Allow-Origin', '*');
+let _options = { headers: headers };
+debugger;
+  await this.http.get<any>('https://localhost:44358/api/Login/GoogleLogin',_options).subscribe(res => {
+    
+    this.login = res;
+    console.log(this.login);
+    if(this.login.message=="Success"){
+      this.router.navigateByUrl('/dashboard');
+    }
+    else{
+      this.toastr.error(this.login.message);
+    }
+
+  }); 
+
+/* var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+if (this.readyState == 4 && this.status == 200) {
+console.log(xhttp.responseText);
+}
+};
+xhttp.open("GET", "https://localhost:44358/api/Login/GoogleLogin", true);
+xhttp.send(); */
+
+}
+
+
 Register(){
   this.router.navigateByUrl('/register');
 }
